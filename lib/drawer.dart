acadcd615988122ce0e042685847403a911fa31f
Stacy Morales <stacymorales049@gmail.com>
2023-03-06 23:15:19 -0400

    Drawer
    Drawer con portada e icono

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'main.dart';
 
 class DrawerMenu extends StatelessWidget {
-  const DrawerMenu({super.key});
+  const DrawerMenu({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,49 +10,20 @@ class DrawerMenu extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-          _buildDrawerHeader(),
-          _buildDrawerItem(icon: Icons.home, text: 'Portada', onTap: () => {
-            Navigator.pushReplacementNamed(context, MyApp.portada)
-          }),
+          const UserAccountsDrawerHeader(
+            accountName: Text("Stacy"),
+            accountEmail: Text("202010229@itla.edu.do"),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: AssetImage("assets/Me.jpg"),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text('Portada'),
+            onTap: () => Navigator.pushReplacementNamed(context, MyApp.portada),
+          ),
         ],
       ),
-    );
-  }
-
-
-  Widget _buildDrawerHeader() {
-    return DrawerHeader(
-        margin: EdgeInsets.zero,
-        padding: EdgeInsets.zero,
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                fit: BoxFit.fill,
-                image:  AssetImage('assests/escena1.webp'))),
-        child: Stack(children: const <Widget>[
-          Positioned(
-              bottom: 12.0,
-              left: 16.0,
-              child: Text("Menú",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w500))),
-        ]));
-  }
-
-  Widget _buildDrawerItem(
-      {required IconData icon, required String text, required GestureTapCallback onTap}) {
-    return ListTile(
-      title: Row(
-        children: <Widget>[
-          Icon(icon),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Text(text),
-          )
-        ],
-      ),
-      onTap: onTap,
     );
   }
 }
